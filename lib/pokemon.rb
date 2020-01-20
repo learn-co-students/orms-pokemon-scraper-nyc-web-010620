0cd1f70853aaaac6e726d7227e317abf378f0fdd
Miles Higbie <miles.higbie@gmail.com>
2020-01-20 12:36:13 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,14 +19,4 @@ class Pokemon
         p1.type = pokemon[0][2]
         return p1
     end 
-
-    # def self.find(num, db)
-    #     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
-    #     new_pokemon = self.new(pokemon)
-    #     new_pokemon.id = pokemon[0][0]
-    #     new_pokemon.name = pokemon[0][1]
-    #     new_pokemon.type = pokemon[0][2]
-    #     new_pokemon.hp = pokemon[0][3]
-    #     return new_pokemon
-    #   end
 end
